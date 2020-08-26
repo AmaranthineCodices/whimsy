366300ad4e467a318a76ecf8f73ff9e152eecc90
AmaranthineCodices <git@amaranthinecodices.me>
2020-08-25 18:41:47 -0700

    some more utility functions

--- src/window.rs
+++ src/window.rs
@@ -1,7 +1,8 @@
 /// Rustified abstraction layer over winapi for interacting with (top-level) windows.
-use winapi::um::winuser::GetWindowRect;
+use winapi::um::winuser;
 
 pub type WindowHandle = winapi::shared::windef::HWND;
+type Win32Rect = winapi::shared::windef::RECT;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Rect {
@@ -9,6 +10,43 @@ pub struct Rect {
     top: i32,
     right: i32,
     bottom: i32,
+}
+
+impl Rect {
+    fn xyxy(left: i32, top: i32, right: i32, bottom: i32) -> Rect {
+        let (x0, x1) = if left < right {
+            (left, right)
+        } else {
+            (right, left)
+        };
+
+        let (y0, y1) = if top < bottom {
+            (top, bottom)
+        } else {
+            (bottom, top)
+        };
+
+        Rect {
+            left: x0,
+            top: y0,
+            right: x1,
+            bottom: y1,
+        }
+    }
+
+    fn xywh(left: i32, top: i32, width: i32, height: i32) -> Rect {
+        let right = left + width;
+        let bottom = top + height;
+
+        Rect::xyxy(left, top, right, bottom)
+    }
+
+    fn wh(&self) -> (i32, i32) {
+        (
+            (self.right - self.left).abs(),
+            (self.bottom - self.top).abs(),
+        )
+    }
 }
 
 #[derive(Debug)]
@@ -23,8 +61,8 @@ impl Window {
 
     pub fn get_rect(&self) -> Result<Rect, ()> {
         unsafe {
-            let mut winapi_rect: winapi::shared::windef::RECT = std::mem::zeroed();
-            evaluate_fallible_winapi!(GetWindowRect(self.handle, &mut winapi_rect));
+            let mut winapi_rect: Win32Rect = std::mem::zeroed();
+            evaluate_fallible_winapi!(winuser::GetWindowRect(self.handle, &mut winapi_rect));
 
             Ok(Rect {
                 left: winapi_rect.left,
@@ -33,5 +71,26 @@ impl Window {
                 bottom: winapi_rect.bottom,
             })
         }
+    }
+
+    pub fn set_rect(&mut self, rect: Rect) -> Result<(), ()> {
+        // Size and position will change, Z order will not. We don't want to activate the window,
+        // and this call should be non-blocking.
+        let flags = winuser::SWP_NOZORDER | winuser::SWP_NOACTIVATE | winuser::SWP_ASYNCWINDOWPOS;
+        let (w, h) = rect.wh();
+
+        unsafe {
+            evaluate_fallible_winapi!(winuser::SetWindowPos(
+                self.handle,
+                std::ptr::null_mut(),
+                rect.left,
+                rect.top,
+                w,
+                h,
+                flags
+            ));
+        }
+
+        Ok(())
     }
 }
