40963a48a8ee5278c9998726cf906b3c134b6c13
AmaranthineCodices <git@amaranthinecodices.me>
2020-08-25 19:21:20 -0700

    Really basic keybind system
    I do not know if this reliably supports super (Windows) modifiers; in
    order to support that I may have to switch away from RegisterHotKey.
    
    Right now this uses Rc<dyn FnMut() -> ()> but it can possibly use Box
    instead, I'm just not able to think it through right now.
    
    Eventually should stop using constants from winapi, I want to abstract
    over all of that for sanity's sake, but for now this works.

---
+++ src/keybind.rs
@@ -0,0 +1,113 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+
+use winapi::um::winuser;
+
+#[derive(Debug, Copy, Clone)]
+pub enum Modifier {
+    Control,
+    Alt,
+    Shift,
+    Super,
+}
+
+fn modifier_to_flag_code(modifier: &Modifier) -> isize {
+    match modifier {
+        Modifier::Control => winuser::MOD_CONTROL,
+        Modifier::Alt => winuser::MOD_ALT,
+        Modifier::Shift => winuser::MOD_SHIFT,
+        Modifier::Super => winuser::MOD_WIN,
+    }
+}
+
+pub struct Keybinds {
+    bind_id_increment: i32,
+    active_binds: HashMap<i32, Rc<dyn FnMut() -> ()>>,
+}
+
+impl Keybinds {
+    pub fn new() -> Keybinds {
+        Keybinds {
+            bind_id_increment: 0,
+            active_binds: HashMap::new(),
+        }
+    }
+
+    // FIXME: Use an actual enum instead of an integer code for better error checking.
+    // Need to figure out how to autogenerate this.
+    pub fn register_keybind(
+        &mut self,
+        key_code: u32,
+        modifiers: Vec<Modifier>,
+        callback: Rc<dyn FnMut() -> ()>,
+    ) -> Result<(), ()> {
+        let id = self.bind_id_increment;
+        let modifier_flags = modifiers.iter().fold(0, |accumulator, modifier| {
+            accumulator | modifier_to_flag_code(modifier)
+        }) | winuser::MOD_NOREPEAT;
+
+        unsafe {
+            evaluate_fallible_winapi!(winuser::RegisterHotKey(
+                std::ptr::null_mut(),
+                id,
+                // cast is safe, the maximum value of modifier_flags is 0x400F
+                modifier_flags as u32,
+                key_code
+            ));
+        }
+
+        log::debug!(
+            "Registered keybind with internal ID {:?}, modifiers {:?}, and virtual keycode {:?}.",
+            id,
+            modifiers,
+            key_code
+        );
+
+        self.active_binds.insert(id, callback);
+        self.bind_id_increment += 1;
+        Ok(())
+    }
+
+    pub fn start_message_loop(mut self) -> Result<(), ()> {
+        log::debug!("Starting keybind message loop.");
+
+        unsafe {
+            let mut msg: winuser::MSG = std::mem::zeroed();
+
+            loop {
+                // Do not use evaluate_fallible_winapi! because GetMessage has different return values than it can accept.
+                let result = winuser::GetMessageW(
+                    &mut msg,
+                    std::ptr::null_mut(),
+                    winuser::WM_HOTKEY,
+                    winuser::WM_HOTKEY,
+                );
+
+                match result {
+                    0 => break,
+                    -1 => {
+                        log::error!(
+                            "Error from GetMessageW: {}",
+                            winapi::um::errhandlingapi::GetLastError(),
+                        );
+                        return Err(());
+                    }
+                    _ => {
+                        debug_assert!(msg.message == winuser::WM_HOTKEY, "The keybind message loop only handles WM_HOTKEY messages, but it has received a message that is not a hotkey message.");
+                        let id = msg.wParam as i32;
+
+                        if !self.active_binds.contains_key(&id) {
+                            log::error!("Unregistered keybind with ID {} was fired.", id);
+                            continue;
+                        }
+
+                        log::trace!("Keybind {} pressed", id);
+                        Rc::get_mut(&mut self.active_binds.get_mut(&id).unwrap()).unwrap()();
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
+// util before all others due to the macros it contains
 mod util;
-mod window;
 
-unsafe extern "system" fn enum_windows_callback(
-    hwnd: winapi::shared::windef::HWND,
-    _: winapi::shared::minwindef::LPARAM,
-) -> i32 {
-    let wnd = window::Window::from_window_handle(hwnd);
-    println!("Window handle {:?}, rect {:?}", hwnd, wnd.get_rect());
-    1
-}
+mod keybind;
+mod window;
 
 fn main() {
     pretty_env_logger::init();
 
-    unsafe {
-        winapi::um::winuser::EnumWindows(Some(enum_windows_callback), 0);
-    }
+    let mut kb = keybind::Keybinds::new();
+    kb.register_keybind(
+        winapi::um::winuser::VK_F1 as u32,
+        vec![
+            keybind::Modifier::Shift,
+            keybind::Modifier::Alt,
+            keybind::Modifier::Control,
+        ],
+        std::rc::Rc::new(|| {
+            println!("Ctrl+Shift+Alt+F1 pressed!");
+        }),
+    )
+    .unwrap();
+
+    kb.start_message_loop().unwrap();
 }
