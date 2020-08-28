d4a28dd256151990ebe0b54f987ff10127602bd8
AmaranthineCodices <git@amaranthinecodices.me>
2020-08-27 22:11:10 -0700

    simple window snapping

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,66 @@ mod util;
 mod keybind;
 mod window;
 
+#[derive(Debug, Copy, Clone)]
+enum PushDirection {
+    Up,
+    Left,
+    Right,
+    Down,
+}
+
+fn slice_rect(direction: PushDirection, rect: window::Rect, slice_factor: f32) -> window::Rect {
+    let (width, height) = rect.wh();
+    let width_slice = ((width as f32) / slice_factor) as i32;
+    let height_slice = ((height as f32) / slice_factor) as i32;
+
+    match direction {
+        PushDirection::Up => window::Rect::xywh(rect.left, rect.top, width, height_slice),
+        PushDirection::Left => window::Rect::xywh(rect.left, rect.top, width_slice, height),
+        PushDirection::Right => window::Rect::xywh(
+            rect.left + width - width_slice,
+            rect.top,
+            width_slice,
+            height,
+        ),
+        PushDirection::Down => window::Rect::xywh(
+            rect.left,
+            rect.top + height - height_slice,
+            width,
+            height_slice,
+        ),
+    }
+}
+
 fn main() {
     pretty_env_logger::init();
 
     let mut kb = keybind::Keybinds::new();
-    kb.register_keybind(
-        winapi::um::winuser::VK_RIGHT as u32,
-        vec![
-            keybind::Modifier::Alt,
-            keybind::Modifier::Control,
-            keybind::Modifier::Super,
-        ],
-        std::rc::Rc::new(|| {
-            if let Some(mut active_window) = window::get_focused_window() {
-                let monitor = active_window.get_monitor();
-                let monitor_work_area = monitor.get_work_area().unwrap();
-                let (monitor_width, _monitor_height) = monitor_work_area.wh();
 
-                let half_rect = window::Rect::xyxy(
-                    monitor_work_area.left + monitor_width / 2,
-                    monitor_work_area.top,
-                    monitor_work_area.right + monitor_width,
-                    monitor_work_area.bottom,
-                );
+    let mut make_push_kb = |key: i32, direction: PushDirection| {
+        kb.register_keybind(
+            key as u32,
+            vec![
+                keybind::Modifier::Alt,
+                keybind::Modifier::Control,
+                keybind::Modifier::Super,
+            ],
+            std::rc::Rc::new(move || {
+                if let Some(mut active_window) = window::get_focused_window() {
+                    let monitor = active_window.get_monitor();
+                    let monitor_work_area = monitor.get_work_area().unwrap();
+                    let pushed_rect = slice_rect(direction, monitor_work_area, 2.0);
+                    active_window.set_rect(pushed_rect).unwrap();
+                }
+            }),
+        )
+        .unwrap();
+    };
 
-                active_window.set_rect(half_rect).unwrap();
-            }
-        }),
-    )
-    .unwrap();
+    make_push_kb(winapi::um::winuser::VK_UP, PushDirection::Up);
+    make_push_kb(winapi::um::winuser::VK_LEFT, PushDirection::Left);
+    make_push_kb(winapi::um::winuser::VK_RIGHT, PushDirection::Right);
+    make_push_kb(winapi::um::winuser::VK_DOWN, PushDirection::Down);
 
     kb.start_message_loop().unwrap();
 }
